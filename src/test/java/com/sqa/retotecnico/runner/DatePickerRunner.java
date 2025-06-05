2522e04531ae01c39b0fb76f578a3b143d3014fe
unknown <jairo>
2025-06-05 17:07:33 -0500

    Ajuste driver Chrome

--- src/test/java/com/sqa/retotecnico/runner/DatePickerRunner.java
+++ src/test/java/com/sqa/retotecnico/runner/DatePickerRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
 
         features = "src/test/resources/features/",
-        tags = " @Scenario2",
+        tags = " @Scenario1",
         glue = "com.sqa.retotecnico.stepDefinitions",
         snippets = CucumberOptions.SnippetType.CAMELCASE
 )
